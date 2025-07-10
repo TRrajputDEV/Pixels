2b7b811d997766c528888ee13a5daf7630e9f104
TRrajputDev <tushartanwar183@gmail.com>
2025-07-10 23:10:01 +0530

    config setup for cors and cookie-parsers

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,26 @@
 import express from 'express'
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+// making the server here from express
 
 const app = express();
 
+// setting up the CORS configration so we can have the URI setup for API request Acceptance.
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials : true
+}))
+// here we are setting the config of what and how much data we gonna get and how..
+app.use(express.json({limit: "16kb"}))
+
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+
+app.use(express.static("public"))
+
+// we are using here app.use as this is an configration of this app - aka CORs, 
+
+app.use (express.cookieParser())
+
+
 export {app} 
+
