98680d9c7de4a9b3f888ad4f9e4964857234fe52
TRrajputDev <tushartanwar183@gmail.com>
2025-07-16 00:26:00 +0530

    Refactor getCurrentUser response to use ApiResponse format

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -263,7 +263,7 @@ const changeCurrentPassword = asyncHandler(async(req, res)=>{
 const getCurrentUser = asyncHandler(async(req, res) =>{
     return res
     .status(200)
-    .json(200, req.user, "Current User fetched")
+    .json(new ApiResponse(200, req.user, "Current user fetched"))
 })
 
 const updateAccountDetails = asyncHandler(async(req, res) =>{
